7071c84a0ff7a080194368b3be446216675e0385
iinaki <llorensinaki22@gmail.com>
2024-05-13 00:19:17 -0300

    agrego psycopg2

--- config/db.py
+++ config/db.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = "postgres+pyscopg2://dvote_user:aD7nniFvRBRiCc4rkSTBWcypCQh18yka@dpg-cp0nkta1hbls73edkjc0-a.ohio-postgres.render.com/dvote"
+DATABASE_URL = "postgresql+pyscopg2://dvote_user:aD7nniFvRBRiCc4rkSTBWcypCQh18yka@dpg-cp0nkta1hbls73edkjc0-a.ohio-postgres.render.com/dvote"
 
 engine = create_engine(DATABASE_URL)
 
